980689d076f27e1519fc389e51c666eb5551874d
ravishmalhotra <rmalhot@umich.edu>
2016-02-16 14:04:13 -0800

    GPIO LED test
    Using the Zynq PS system to control GPIO pins through interrupt etc.

---
+++ Debug/src/helloworld.d
@@ -0,0 +1,48 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../gpio_led_bsp/ps7_cortexa9_0/include/xstatus.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../gpio_led_bsp/ps7_cortexa9_0/include/xstatus.h:
